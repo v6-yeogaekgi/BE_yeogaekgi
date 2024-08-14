0194f1c284884366761c79477fb25ea0c099b8f2
hyunjin9603 <hyunjin9603@gmail.com>
2024-08-14 15:26:46 +0900

    Initial commit

---
+++ src/main/java/com/v6/yeogaekgi/YeogaekgiApplication.java
@@ -0,0 +1,13 @@
+package com.v6.yeogaekgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeogaekgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeogaekgiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/v6/yeogaekgi/YeogaekgiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v6.yeogaekgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeogaekgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
