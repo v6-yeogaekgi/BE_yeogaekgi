6b3f6bd6a4733f698d168230e5425630fea3bc72
shseo232 <shseo232@gmail.com>
2024-08-16 02:18:17 +0900

    feat: Review Entity에 컬럼 추가 (images,score,content,status)

--- src/main/java/com/v6/yeogaekgi/review/entity/Review.java
+++ src/main/java/com/v6/yeogaekgi/review/entity/Review.java
@@ -1,17 +1,34 @@
 package com.v6.yeogaekgi.review.entity;
 
+import com.v6.yeogaekgi.member.entity.Member;
+import com.v6.yeogaekgi.util.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.aspectj.apache.bcel.classfile.Code;
+
+import java.sql.Timestamp;
 
 @Entity
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Review {
+public class Review extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "review_no")
     private Long id;
+
+    private String images;
+
+    @Column(nullable=false)
+    private int score;
+
+    @Column(nullable=false)
+    private String content;
+
+    @Column(nullable=false)
+    private int status=0;
+
 }
