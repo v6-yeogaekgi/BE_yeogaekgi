a60f9d6af5813660157eeb64a3c9aee4b55e1825
hyunjin9603 <hyunjin9603@gmail.com>
2024-08-19 12:36:16 +0900

    feat:ReviewDel

--- src/main/java/com/v6/yeogaekgi/review/controller/ReviewController.java
+++ src/main/java/com/v6/yeogaekgi/review/controller/ReviewController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/com/v6/yeogaekgi/review/repository/ReviewRepository.java
+++ src/main/java/com/v6/yeogaekgi/review/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package com.v6.yeogaekgi.review.repository;
 
 import com.v6.yeogaekgi.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review,Long> {
 }
