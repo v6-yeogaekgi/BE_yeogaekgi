e7544c3be17b2189c42630f2ee92c4ce5d67fcf0
hyunjin9603 <hyunjin9603@gmail.com>
2024-08-19 14:03:56 +0900

    feat:config, MemberDetailsImpl & MemberDetailsService

---
+++ src/main/java/com/v6/yeogaekgi/config/WebSecurityConfig.java
@@ -0,0 +1,104 @@
+package com.v6.yeogaekgi.config;
+
+import com.v6.yeogaekgi.member.repository.MemberRepository;
+import com.v6.yeogaekgi.security.MemberDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig implements WebMvcConfigurer {
+    private final TokenProvider tokenProvider;
+    private final MemberDetailsServiceImpl memberDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final MemberRepository memberRepository;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationFilter authenticationFilter() throws Exception {
+        AuthenticationFilter filter = new AuthenticationFilter(tokenProvider,refreshTokenRepository,memberRepository);
+        filter.setAuthenticationManagerResolver(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
+
+    @Bean
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "Access-Control-Allow-Origin","Refresh"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization","Refresh"));
+        configuration.setMaxAge(1800L);
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public AuthorizationFilter authorizationFilter() {
+        return new AuthorizationFilter(tokenProvider, memberDetailsService,refreshTokenRepository);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // CORS 설정
+        http.cors((cors) -> cors.configurationSource(configurationSource()));
+
+        // CSRF 설정
+        http.csrf((csrf) -> csrf.disable());
+
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement((sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                .requestMatchers("/members/**").permitAll()
+                                .requestMatchers(HttpMethod.GET).permitAll()
+                                .anyRequest().authenticated()
+        );
+
+
+        // 필터 관리
+        http.addFilterBefore(authorizationFilter(), AuthenticationFilter.class);
+        http.addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
--- src/main/java/com/v6/yeogaekgi/security/MemberDetailsServiceImpl.java
+++ src/main/java/com/v6/yeogaekgi/security/MemberDetailsServiceImpl.java
@@ -1,12 +1,24 @@
 package com.v6.yeogaekgi.security;
 
+import com.v6.yeogaekgi.member.entity.Member;
 import com.v6.yeogaekgi.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MemberDetailsServiceImpl{
+public class MemberDetailsServiceImpl implements UserDetailsService {
     private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername (String nickname)throws UsernameNotFoundException {
+        Member member = memberRepository.findByNickname(nickname)
+                .orElseThrow(()->new UsernameNotFoundException("유저를 찾을 수 없습니다." + nickname));
+
+        return new MemberDetailsImpl(member);
+    }
 
 }
