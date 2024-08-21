42c1e4f646ca4c23f86d9875d0d1e81009cd9f65
mint723 <neu990112@gmail.com>
2024-08-21 15:48:46 +0900

    fix: dto structure

--- src/main/java/com/v6/yeogaekgi/payTrack/dto/PayTrackDTO.java
+++ src/main/java/com/v6/yeogaekgi/payTrack/dto/PayTrackDTO.java
@@ -12,8 +12,12 @@ import java.sql.Timestamp;
 public class PayTrackDTO {
     private long userCardNo;
 
-    // pay_date + tran_date 통합 관리
+    // 통합 관리
     private Timestamp datetime;
+
+    private Integer transitBalanceSnap;
+
+    private Integer payBalanceSnap;
 
     // [ 결제(Payment) 내역 ]
     private Integer pno;
@@ -26,24 +30,17 @@ public class PayTrackDTO {
 
     private String serviceName;
 
-    private Integer payment_transit_balance_snap;
-
-    private Integer payment_pay_balance_snap;
 
     // [ 거래(Transaction) 내역 ]
     private Integer tno;
 
     private Integer tranType; // 0전환 | 1충전 | 2환급
 
-    private Integer transfer_type; // 0페이 -> 교통 | 교통 -> 페이
+    private Integer transferType; // 0페이 -> 교통 | 교통 -> 페이 // transfer_type
 
-    private Integer krw_amount;
+    private Integer krwAmount; //krw_amount
 
-    private Integer foreign_amount;
-
-    private Integer transaction_transit_balance_snap;
-
-    private Integer transaction_pay_balance_snap;
+    private Integer foreignAmount; //foreign_amount
 
     // 결제인지 거래인지
     private boolean isPayment;
