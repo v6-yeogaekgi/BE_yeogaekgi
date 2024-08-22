e2f6a6365825000a35329a5e21230a7ac1ff54ee
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-08-22 12:01:35 +0900

    Feat : comment register /w security

--- src/main/java/com/v6/yeogaekgi/community/controller/CommentController.java
+++ src/main/java/com/v6/yeogaekgi/community/controller/CommentController.java
@@ -2,13 +2,18 @@ package com.v6.yeogaekgi.community.controller;
 
 import com.v6.yeogaekgi.community.dto.CommentDTO;
 import com.v6.yeogaekgi.community.service.CommentService;
+import com.v6.yeogaekgi.kiosk.dto.KioskRequestDTO;
+import com.v6.yeogaekgi.kiosk.dto.KioskResponseDTO;
+import com.v6.yeogaekgi.security.MemberDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/community/comment")
@@ -29,18 +34,18 @@ public class CommentController {
     }
 
     @PostMapping("/{postId}")
-    public ResponseEntity<Long> registerComment(@PathVariable("postId") Long postId, @RequestBody CommentDTO commentDTO){
+    public ResponseEntity<Long> registerComment(@PathVariable("postId") Long postId, @RequestBody CommentDTO commentDTO,@AuthenticationPrincipal MemberDetailsImpl memberDetails){
         commentDTO.setPostId(postId);
         log.info("----------------register commnet-------------------");
         log.info("commentDTO : "+commentDTO);
-        Long commentId = commentService.register(commentDTO);
+        Long commentId = commentService.register(commentDTO,memberDetails.getMember());
         return new ResponseEntity<>(commentId,HttpStatus.OK);
 
     }
 
     @PutMapping("/{postId}/{commentId}")
     public ResponseEntity<Long> modifyComment(@PathVariable("commentId") Long commentId,
-                                             @RequestBody CommentDTO commentDTO){
+                                             @RequestBody CommentDTO commentDTO, @AuthenticationPrincipal MemberDetailsImpl memberDetails){
         commentDTO.setCommentId(commentId);
         log.info("---------------modify Comment--------------" + commentId);
         log.info("commentDTO: " + commentDTO);
@@ -49,6 +54,7 @@ public class CommentController {
 
         return new ResponseEntity<>(commentId, HttpStatus.OK);
     }
+
 
     @DeleteMapping("/{postId}/{commentId}")
     public ResponseEntity<Long> removeComment( @PathVariable Long commentId){
