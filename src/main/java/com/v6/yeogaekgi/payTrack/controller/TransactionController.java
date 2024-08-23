61aa3253207930347992cddee6de2b0042c23716
JaneKim <jane.kim.279@gmail.com>
2024-08-23 15:50:41 +0900

    fix: cors 추가

--- src/main/java/com/v6/yeogaekgi/payTrack/controller/TransactionController.java
+++ src/main/java/com/v6/yeogaekgi/payTrack/controller/TransactionController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/transaction")
 @Log4j2
+@CrossOrigin(origins = {"*"})
 @RequiredArgsConstructor
 public class TransactionController {
     @Autowired
