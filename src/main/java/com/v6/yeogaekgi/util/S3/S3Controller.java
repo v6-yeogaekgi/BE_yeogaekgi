3b20f1e0ead98ec104fc5280a9419bc98363cfea
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-08-23 13:44:38 +0900

    Feat : PostController register with images

--- src/main/java/com/v6/yeogaekgi/util/S3/S3Controller.java
+++ src/main/java/com/v6/yeogaekgi/util/S3/S3Controller.java
@@ -2,6 +2,7 @@ package com.v6.yeogaekgi.util.S3;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
-
+@Log4j2
 @RestController
 @RequestMapping("/image")
 @RequiredArgsConstructor
@@ -19,11 +20,15 @@ public class S3Controller {
 
     @PostMapping("")
     public ResponseEntity<List<Map<String, String>>> uploadImage(@RequestParam List<MultipartFile> multipartFile) {
+        log.info("----------------multipartFile uploadImage-------------------");
+        log.info("multipartFile : "+multipartFile);
         return new ResponseEntity<>(awsS3Service.uploadImage(multipartFile), HttpStatus.OK);
     }
 
     @DeleteMapping("")
     public ResponseEntity<String> deleteImage(@RequestParam String fileName) {
+        log.info("----------------multipartFile deleteImage-------------------");
+        log.info("fileName : "+fileName);
         awsS3Service.deleteImage(fileName);
         return new ResponseEntity<>(fileName,HttpStatus.OK);
     }
