b621d5f05e4c5059f1bd72ac6e591df21d12b4e6
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-08-25 16:11:06 +0900

    Feat : Add member controller @CrossOrigin

--- src/main/java/com/v6/yeogaekgi/member/controller/MemberController.java
+++ src/main/java/com/v6/yeogaekgi/member/controller/MemberController.java
@@ -10,12 +10,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+@CrossOrigin(origins = {"*"})
 
 @RestController
 @RequiredArgsConstructor
