233af5c980ee960cc21a5a03a866eae277995254
mint723 <neu990112@gmail.com>
2024-08-27 19:06:15 +0900

    test: modify star

--- src/test/java/com/v6/yeogaekgi/service/UserCardServiceTests.java
+++ src/test/java/com/v6/yeogaekgi/service/UserCardServiceTests.java
@@ -2,6 +2,7 @@ package com.v6.yeogaekgi.service;
 
 import com.v6.yeogaekgi.card.dto.UserCardDTO;
 import com.v6.yeogaekgi.card.service.UserCardService;
+import com.v6.yeogaekgi.repository.CommonTest.CommonTests;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-public class UserCardServiceTests {
+public class UserCardServiceTests extends CommonTests {
     @Autowired
     private UserCardService userCardService;
 
@@ -20,5 +21,17 @@ public class UserCardServiceTests {
         System.out.println(userCardByCardId);
 
         assertNotNull(userCardByCardId);
+    }
+
+    @Test
+    public void testModifyStarred(){
+        long userCardNo = 400;
+        long member_no = 1396;
+
+        UserCardDTO cardDTO = UserCardDTO.builder()
+                .userCardId(userCardNo)
+                .memberId(member_no)
+                .build();
+        userCardService.changesUserCardStarred(cardDTO);
     }
 }
